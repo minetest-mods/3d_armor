d4a52acf030f494a5c6366922fb339fc98fef351
tenplus1 <tenplus1@users.noreply.github.com>
2021-11-11 11:01:50 +0000

    add fire protection to nether armor
    add fire protection to nether armor

--- 3d_armor/armor.lua
+++ 3d_armor/armor.lua
@@ -882,6 +882,7 @@ if armor.materials.nether then
 	--  @grp armor_head 1
 	--  @grp armor_heal 14
 	--  @grp armor_use 200
+	--  @grp armor_fire 1
 	--  @armorgrp fleshy 18
 	--  @damagegrp cracky 3
 	--  @damagegrp snappy 2
@@ -889,7 +890,7 @@ if armor.materials.nether then
 	armor:register_armor("3d_armor:helmet_nether", {
 		description = S("Nether Helmet"),
 		inventory_image = "3d_armor_inv_helmet_nether.png",
-		groups = {armor_head=1, armor_heal=14, armor_use=100},
+		groups = {armor_head=1, armor_heal=14, armor_use=100, armor_fire=1},
 		armor_groups = {fleshy=18},
 		damage_groups = {cracky=3, snappy=2, level=3},
 	})
@@ -900,6 +901,7 @@ if armor.materials.nether then
 	--  @grp armor_torso 1
 	--  @grp armor_heal 14
 	--  @grp armor_use 200
+	--  @grp armor_fire 1
 	--  @armorgrp fleshy 25
 	--  @damagegrp cracky 3
 	--  @damagegrp snappy 2
@@ -907,7 +909,7 @@ if armor.materials.nether then
 	armor:register_armor("3d_armor:chestplate_nether", {
 		description = S("Nether Chestplate"),
 		inventory_image = "3d_armor_inv_chestplate_nether.png",
-		groups = {armor_torso=1, armor_heal=14, armor_use=200},
+		groups = {armor_torso=1, armor_heal=14, armor_use=200, armor_fire=1},
 		armor_groups = {fleshy=25},
 		damage_groups = {cracky=3, snappy=2, level=3},
 	})
@@ -918,6 +920,7 @@ if armor.materials.nether then
 	--  @grp armor_legs 1
 	--  @grp armor_heal 14
 	--  @grp armor_use 200
+	--  @grp armor_fire 1
 	--  @armorgrp fleshy 25
 	--  @damagegrp cracky 3
 	--  @damagegrp snappy 2
@@ -925,7 +928,7 @@ if armor.materials.nether then
 	armor:register_armor("3d_armor:leggings_nether", {
 		description = S("Nether Leggings"),
 		inventory_image = "3d_armor_inv_leggings_nether.png",
-		groups = {armor_legs=1, armor_heal=14, armor_use=200},
+		groups = {armor_legs=1, armor_heal=14, armor_use=200, armor_fire=1},
 		armor_groups = {fleshy=25},
 		damage_groups = {cracky=3, snappy=2, level=3},
 	})
@@ -936,6 +939,7 @@ if armor.materials.nether then
 	--  @grp armor_feet 1
 	--  @grp armor_heal 14
 	--  @grp armor_use 200
+	--  @grp armor_fire 1
 	--  @armorgrp fleshy 18
 	--  @damagegrp cracky 3
 	--  @damagegrp snappy 2
@@ -943,7 +947,7 @@ if armor.materials.nether then
 	armor:register_armor("3d_armor:boots_nether", {
 		description = S("Nether Boots"),
 		inventory_image = "3d_armor_inv_boots_nether.png",
-		groups = {armor_feet=1, armor_heal=14, armor_use=200},
+		groups = {armor_feet=1, armor_heal=14, armor_use=200, armor_fire=1},
 		armor_groups = {fleshy=18},
 		damage_groups = {cracky=3, snappy=2, level=3},
 	})
